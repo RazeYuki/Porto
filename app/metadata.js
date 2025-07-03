6fc00176e5405adc4a1d84ce7c65bfb60f913e8c
RazeYuki <130343333+RazeYuki@users.noreply.github.com>
2025-07-04 05:17:37 +0700

    commit

--- app/metadata.js
+++ app/metadata.js
@@ -2,15 +2,15 @@
 // TIDAK ADA 'use client' DI SINI
 
 export const metadata = {
-  title: 'Dika Portfolio', // <-- Ganti dengan nama portofolio Anda
-  description: 'Portofolio Dika sebagai seorang Frontend Developer', // <-- Ganti dengan deskripsi Anda
-  keywords: ['frontend', 'developer', 'react', 'nextjs', 'portfolio'],
+  title: 'RazeYuki', // <-- Ganti dengan nama portofolio Anda
+  description: 'My Portfolio', // <-- Ganti dengan deskripsi Anda
+  keywords: ['BigData', 'developer', 'react', 'nextjs', 'portfolio'],
   authors: [{ name: 'Dika' }],
   openGraph: {
-    title: 'Dika Portfolio',
-    description: 'Portofolio Dika sebagai seorang Frontend Developer',
+    title: 'RazeYuki',
+    description: 'My Portfolio',
     url: 'https://yourportfolio.com',
-    siteName: 'Dika Portfolio',
+    siteName: 'RazeYuki',
     images: [
       {
         url: 'https://yourportfolio.com/og-image.jpg',
@@ -23,8 +23,8 @@ export const metadata = {
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Dika Portfolio',
-    description: 'Portofolio Dika sebagai seorang Frontend Developer',
+    title: 'Razeyuki',
+    description: 'My Portfolio',
     creator: '@yourtwitterhandle',
     images: ['https://yourportfolio.com/twitter-image.jpg'],
   },
